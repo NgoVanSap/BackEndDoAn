37ff2f4fa16809929ea005b31029beefb55aaf10
Ngô Văn Sáp <100812540+NgoVanSap@users.noreply.github.com>
2023-04-10 02:45:47 +0700

    CMD

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,6 +26,7 @@ class CreateUsersTable extends Migration
             $table->integer('trangThai');
             $table->integer('phanQuyen');
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('last_seen')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
