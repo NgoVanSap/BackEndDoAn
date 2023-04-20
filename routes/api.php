3f99b704033c9b6cae70bfa8544370bb4a66565a
Ngô Văn Sáp <100812540+NgoVanSap@users.noreply.github.com>
2023-04-20 17:38:05 +0700

    CMD

--- routes/api.php
+++ routes/api.php
@@ -25,10 +25,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('cors')->group(function () {
 
     Route::middleware('CheckUserRole')->group(function () {
-        Route::middleware(['auth.api'])->group(function () {
-            Route::post('post/category', [CategoryController::class, 'createCategory'])->name('createCategory');
-        });
-
+        Route::post('post/category', [CategoryController::class, 'createCategory'])->name('createCategory');
     });
 
     Route::middleware('auth:sanctum')->group(function () {
